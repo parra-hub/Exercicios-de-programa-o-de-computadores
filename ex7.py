c65a38ab0d2194425221367fac2cd9c8881f31ae
parra-hub <parra@alunos.utfpr.edu.br>
2025-09-19 17:03:30 -0300

    Delete ex7.py

--- ex7.py
+++
@@ -1,8 +0,0 @@
-tupla=(1,1,2,3,1,2,3,4,1,2,3)
-usuario=int(input('informe um valor'))
-cont=0
-for i in range(len(tupla)):
-    if usuario == tupla[i]:
-        cont=cont+1
-
-print('O numero:',usuario,'aparece:',cont,'vezes na lista')
