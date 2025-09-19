ee52b0f4f98906270c6c713f1c90830df5198e27
parra-hub <parra@alunos.utfpr.edu.br>
2025-09-19 17:03:37 -0300

    Delete ex8.py

--- ex8.py
+++
@@ -1,8 +0,0 @@
-vogais = ('a', 'e', 'i', 'o', 'u')
-cont=0
-valor=input('digite uma vogal:')
-for i,numero in enumerate(vogais):
-    if valor == numero:
-        cont=i
-
-print('A vogal',valor,'esta na posição',cont)
